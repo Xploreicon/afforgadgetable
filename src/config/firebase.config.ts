60856f463b0f07440406b4ea1306ecad114e251b
spirit-mac <sprtbeing@gmail.com>
2024-12-23 07:00:55 -0800

    second commit

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,15 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC9F5kC3JAGMgngyNvZZ0hZZLKfnlhJTDs",
-    authDomain: "afforgadgetable.firebaseapp.com",
-    projectId: "afforgadgetable",
-    storageBucket: "afforgadgetable.firebasestorage.app",
-    messagingSenderId: "734738217644",
-    appId: "1:734738217644:web:83d19d32ca7a2920db7d09",
-    measurementId: "G-92P8TPM7EJ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
 export { db };
